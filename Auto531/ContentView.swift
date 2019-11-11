e7fa03fbe4749703729a010f6f23c3affb33ecf0
mattwind <matt.wind@live.com>
2019-11-11 12:02:17 -0700

    Initial Commit

---
+++ Auto531/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Auto531
+//
+//  Created by Matt Wind on 2019-11-11.
+//  Copyright © 2019 Matt Wind. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
